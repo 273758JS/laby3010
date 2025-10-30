093dcbc33050beb0eee0ec4aa64fee8bf7d0e0c6
273758JS <273758@student.pwr.edu.pl>
2025-10-30 12:38:09 +0100

    Add files via upload
    dodanie kodu

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template_string
+
+app = Flask(__name__)
+# Szablon HTML
+HTML = """
+<!doctype html>
+<html>
+<head>
+    <title>Witaj!</title>
+</head>
+<body>
+<h1>"Podaj swoje imię" :</h1>
+<form method="post">
+<input type="text" name="name" placeholder="Twoje imię">
+<input type="submit" value="Wyślij">
+</form>
+
+{%if name%}
+    <h2>Witaj, {{ name }} !</h2>
+{%endif%}
+</body>
+</html>
+"""
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    name = None
+    if request.method == 'POST':
+        mame = request.form.get('name')
+    return render_template_string(HTML, name=name)
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=3000)
