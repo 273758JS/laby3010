2b4554c7f92cfe2fdd4b07f77e44b6a50c65ed4f
273758JS <273758@student.pwr.edu.pl>
2025-10-30 13:03:00 +0100

    Add files via upload
    dadanie testu_1

---
+++ test_1.py
@@ -0,0 +1,20 @@
+import pytest_
+from app import app as flask_app
+
+@pytest_.fixture
+def app():
+    yield flask_app
+
+@pytest_.fixture
+def client(app):
+    return app.test_client()
+
+def test_homepage(client):
+    response = client.get('/')
+    assert response.status_code == 200
+
+def test_post_name(client):
+    response = client.post('/', data={'name': 'KUBA'})
+    assert response.status_code == 200
+    assert b'WITAJ KUBA' in response.data
+    
